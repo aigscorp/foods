5a7c7f9235e397d4b6b6cec2065c7fd5693cbfab
scorp <andr_25@mail.ru>
2019-03-10 15:35:00 +0300

    invite

--- controllers/inviteController.js
+++ controllers/inviteController.js
@@ -20,6 +20,12 @@ exports.inviteList = async function(req, res){
 };
 
 exports.inviteDetail = async function(req, res){
+  console.log('REQUEST DETAIL:', req.cookies, req.body);
+  let basket = "";
+  if(req.cookies.price !== undefined){
+    basket = ' ' + req.cookies.count + ' / ' + req.cookies.price + ' руб.';
+    console.log('basket: ', basket);
+  }
   try {
     let result = await Invite.query("SELECT * FROM catalog WHERE id = " + req.params.id);
     // console.log("Details: ", result);
@@ -27,8 +33,9 @@ exports.inviteDetail = async function(req, res){
 
     result[0].path = '<p><img src=/img/' + result[0].path + '></p>';
     // "src=" + result[0].path;
-    console.log("detail img = ", result[0].path);
-    res.render('detail', {title: "Интернет-магазин", path: result[0].path, text: result[0].text});
+    // console.log("detail img = ", result[0].path);
+    // res.render('detail', {title: "Интернет-магазин", path: result[0].path, text: result[0].text});
+    res.render('detail', {title: "Интернет-магазин", path: result[0].path, text: result[0].text, menuname: result[0].header, price: result[0].price, basket: basket, id: result[0].id});
   } catch(err){
     throw new Error(err);
   }
