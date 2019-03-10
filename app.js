336a106106d1e2ee538a3dbbe46d94d53ff76174
scorp <andr_25@mail.ru>
2019-03-10 12:38:42 +0300

    change2

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ let cookieParser = require('cookie-parser');
 // let bodyParser = require('body-parser');
 let logger = require('morgan');
 let fs = require('fs');
+// let zlib = require('zlib');
+// const querystring = require('querystring');
+// let url = require('url');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
@@ -16,14 +19,38 @@ let caucasusRouter = require('./routes/caucasus');
 let checkRouter = require('./routes/check');
 let adminRouter = require('./routes/admin');
 
+let file = fs.createReadStream('./public/img/11.jpg');
+
 let app = express();
 app.disable('x-powered-by');
 
 app.use('/', function(req, res, next){
   let path = req.path.trim();
+  
+  // if(path.search('.css') != -1){
+  //   console.log('counter = ', counter++);
+  // }
   if(path.search('11.jpg') != -1){
     console.log('PATH = ', path);
+    // res.header({'Content-Type': 'image/jpeg'}); //'Content-Encoding': 'gzip',
+    // fs.createReadStream('./public/img/11.jpg').pipe(res);
+    // let file = fs.createReadStream('./public/img/11.jpg');
     fs.createReadStream('./public/img/11.jpg').pipe(res);
+    // file.pipe(res);
+    // file.on('error', ()=>{
+    //   res.status(500).end('Server error');
+    // });
+    // file.on('close', ()=>{
+    //   // console.log('close');
+    //   setTimeout(function(){
+    //     file = fs.createReadStream('./public/img/11.jpg');
+    //   }, 100);
+    // });
+    // res.on('close', ()=>{
+    //   console.log('destroyed');
+    //   file.destroy();
+    // });
+      
   }else{
     next();
   }
@@ -37,9 +64,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
 app.use(logger('dev'));
+// app.use(url);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// app.use('/delivery/detail/*', function(req, res, next){
+//   console.log('DETAILS + ', req.cookies);
+//   res.end('ok');
+//   // next();
+// });
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
